96eb4309090281f5575dbfd5b1e642ecb1b1f6d2
meamanbaba <meamanbaba@gmail.com>
2022-10-06 16:30:21 +0530

    update
    update

--- crud/display.php
+++ crud/display.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body><center>
+<body>
+    <h1>view all data</h1>
+    <center>
 
 
     <table border="2"  >
